55cbdc4f95fe30c5ec3f8b2cbe142579069fd62d
Mark D. Lalich <mdlalich@gmail.com>
2023-08-08 19:13:07 -0700

    basic FE

---
+++ src/pages/home.js
@@ -0,0 +1,21 @@
+import { useLoaderData, Form } from 'react-router-dom'
+
+
+const Home = (props) => {
+
+        const person = useLoaderData()
+   
+    return <div className='person'>
+        <h1>Bookmark Name</h1>
+        <h2>BookMark Title</h2>
+    
+      
+
+<h2>Update </h2>
+
+
+</div>
+}
+
+
+export default Home;
