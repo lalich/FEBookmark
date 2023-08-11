851167e00717667c52887b47d98220409d0ab5a0
Mark D. Lalich <mdlalich@gmail.com>
2023-08-10 17:30:36 -0700

    all da files

---
+++ src/components/Bookmark.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+function Bookmark(props){
+    return <nav className="nav">
+        <Link to="/">
+            <div>People App</div>
+        </Link>
+    </nav>
+
+}
+
+export default Bookmark
