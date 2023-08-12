78e90b00bbfe8862f723783e14764f558406817f
abanobmorkos1 <abanobmorkos11@gmail.com>
2023-08-12 12:54:21 -0400

    css animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,23 +6,24 @@ function Index(props){
     return (
         <div className="Home">
             <div className="crtfrm">
-            <h2>Add a Bookmark</h2>
+            <h2 className="bm">Add a Bookmark</h2>
             <Form className="form" action="/create" method="post">
                 <input type="text" name="name" placeholder="name"/>
                 <input type="text" name="url"  placeholder="ONLY the website name"required/>
                 <input type="submit" value="Create bookmark"/>
             </Form>
             </div>
-            {data.map((item, index) => <div id="crtfrm" key={index} className="bookmark">
-
-            <h1><a href={`https://${item.url}.com`}>{item.name}</a></h1>
-            <Link to={`/${item._id}`}>
-                <button className="edit">Edit {item.name}</button> 
-            </Link> 
-                <Form action={`/delete/${item._id}`} method="post">
-                <input className="delete" type="submit" value="X"/>
-            </Form>  
-        </div>)}
+            <div id="main">
+                {data.map((item, index) => <div id="crtfrm" key={index} className="bookmark">
+                <h1><a href={`https://${item.url}.com`}>{item.name}</a></h1>
+                <Link to={`/${item._id}`}>
+                    <button className="edit">Edit {item.name}</button>
+                </Link>
+                    <Form action={`/delete/${item._id}`} method="post">
+                    <input className="delete" type="submit" value="X"/>
+                </Form>
+                </div>)}
+            </div>
         </div>
     )
 }
