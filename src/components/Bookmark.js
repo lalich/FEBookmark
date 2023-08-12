2018aa76d5c3dd33d8de7924f4c0dcb221c37ae5
JoyBoyCr7 <Rnyongkah@gmail.com>
2023-08-11 20:40:32 -0500

    Full crud

--- src/components/Bookmark.js
+++ src/components/Bookmark.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 function Bookmark(props){
     return <nav className="nav">
         <Link to="/">
-            <div>People App</div>
+            <div>book App</div>
         </Link>
     </nav>
 
